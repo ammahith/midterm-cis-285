491cab80fb3a95b6e670fe48654094a434ece3ab
Abdullah Mahith <ammahith@umich.edu>
2025-02-27 14:50:17 -0500

    chnages

--- factorialcalculator.java
+++ factorialcalculator.java
@@ -1,7 +1,7 @@
 public class factorialcalculator {
     public static void main(String[] args) {
         int num = 8;  
-        int factorial = 2;
+        int factorial = 4;
 
         for (int i = 1; i <= num; i++) { 
             factorial *= i;  
