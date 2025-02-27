732921876d2a30e368fc6909211434e994aab751
Abdullah Mahith <ammahith@umich.edu>
2025-02-27 14:14:57 -0500

    chnages int number value

--- factorialcalculator.java
+++ factorialcalculator.java
@@ -1,10 +1,10 @@
 public class factorialcalculator {
     public static void main(String[] args) {
-        int num = 5;  // Predefined number to calculate factorial
+        int num = 6;  
         int factorial = 1;
 
-        for (int i = 1; i <= num; i++) { // Loop through numbers
-            factorial *= i;  // Multiply current number
+        for (int i = 1; i <= num; i++) { 
+            factorial *= i;  
         }
 
         System.out.println("Factorial of " + num + " is: " + factorial);
