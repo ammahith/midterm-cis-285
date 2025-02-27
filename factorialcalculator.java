b1cfe2ff1ce72c7e3bcb656ebd76e25e43bf536d
Abdullah Mahith <ammahith@umich.edu>
2025-02-27 14:35:57 -0500

    chnaged int values

--- factorialcalculator.java
+++ factorialcalculator.java
@@ -1,7 +1,7 @@
 public class factorialcalculator {
     public static void main(String[] args) {
-        int num = 6;  
-        int factorial = 1;
+        int num = 8;  
+        int factorial = 2;
 
         for (int i = 1; i <= num; i++) { 
             factorial *= i;  
